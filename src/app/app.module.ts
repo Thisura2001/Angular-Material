1f91649f565fc8f9729b2887af76e66240ac0c7b
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2024-09-24 15:36:08 +0530

    create components and install angular material

---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';  // Required for Material animations
+import { MatButtonModule } from '@angular/material/button';  // Material Button Module
+import { MatIcon } from '@angular/material/icon';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatToolbarRow } from '@angular/material/toolbar';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { MatFormField } from '@angular/material/form-field';
+import { MatHint } from '@angular/material/form-field';
+import { MatLabel } from '@angular/material/form-field';
+import {MatStepperModule} from '@angular/material/stepper';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatSlideToggleModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIcon,
+    MatToolbar,
+    MatToolbarRow,
+    MatFormField,
+    MatHint,
+    MatLabel,
+    MatStepperModule
+  ],
+  providers: [
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
